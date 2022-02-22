c8e2be8a5709b9555acf0de85cec3c313ac34881
emirisawesome <emirisaev2000@gmail.com>
2022-02-22 11:19:40 +0600

    header

--- frontend/src/comonents/Header/Header.jsx
+++ frontend/src/comonents/Header/Header.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './Header.module.css'
+import { FaTwitter } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
 
 const Header = () => {
 	return (
@@ -8,9 +11,26 @@ const Header = () => {
 			<div className={styles.container}>
 				<nav className={styles.topNav}>
 					<ul className={styles.navLeft}>
-						<a href="http://" target="_blank" ></a>
-						<a href="http://" target="_blank" ></a>
-						<a href="http://" target="_blank" ></a>
+						<a href="http://" target="_blank" >
+							<FaFacebookF className={styles.topnav} />
+						</a>
+						<a href="http://" target="_blank" >
+							<FaTwitter className={styles.topnav} />
+						</a>
+						<a href="http://" target="_blank" >
+							<FaYoutube className={styles.topnav} />
+						</a>
+					</ul>
+					<ul className={styles.navLeft}>
+						<a href="http://" target="_blank" >
+							<FaFacebookF className={styles.topnav} />
+						</a>
+						<a href="http://" target="_blank" >
+							<FaTwitter className={styles.topnav} />
+						</a>
+						<a href="http://" target="_blank" >
+							<FaYoutube className={styles.topnav} />
+						</a>
 					</ul>
 				</nav>
 			</div>
