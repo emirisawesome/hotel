96aa9fb6c9e8184012c4a0a467466e9a65440b73
emirisawesome <emirisaev2000@gmail.com>
2022-02-28 12:41:59 +0600

    jdhajkh/jsdahj

--- backend/gostinica/models.py
+++ backend/gostinica/models.py
@@ -6,32 +6,35 @@ from django.contrib.auth.models import User
 class Menu(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
-    descriptions = models.CharField(max_length=100)
+    descriptions = models.CharField(max_length=1200)
+
 
 s = (
-    ('ekonomy','ekonomy'),
-    ('standart','standart'),
-    ('lux','lux'),
-    ('penthaus','penthaus'),
-    ('prezident','prezident')
+    ("ekonomy", "ekonomy"),
+    ("standart", "standart"),
+    ("lux", "lux"),
+    ("penthaus", "penthaus"),
+    ("prezident", "prezident"),
 )
 
 times = (
-    (datetime.datetime.now() + datetime.timedelta(days=1),'1'),
-    (datetime.datetime.now() + datetime.timedelta(days=2),'2'),
-    (datetime.datetime.now() + datetime.timedelta(days=3),'3'),
-    (datetime.datetime.now() + datetime.timedelta(days=4),'4'),
-    (datetime.datetime.now() + datetime.timedelta(days=5),'5'),
-    (datetime.datetime.now() + datetime.timedelta(days=6),'6'),
-    (datetime.datetime.now() + datetime.timedelta(days=7),'7'),
+    ((datetime.datetime.now() + datetime.timedelta(days=1)).strftime("%Y-%m-%d"), "1"),
+    ((datetime.datetime.now() + datetime.timedelta(days=2)).strftime("%Y-%m-%d"), "2"),
+    ((datetime.datetime.now() + datetime.timedelta(days=3)).strftime("%Y-%m-%d"), "3"),
+    ((datetime.datetime.now() + datetime.timedelta(days=4)).strftime("%Y-%m-%d"), "4"),
+    ((datetime.datetime.now() + datetime.timedelta(days=5)).strftime("%Y-%m-%d"), "5"),
+    ((datetime.datetime.now() + datetime.timedelta(days=6)).strftime("%Y-%m-%d"), "6"),
+    ((datetime.datetime.now() + datetime.timedelta(days=7)).strftime("%Y-%m-%d"), "7"),
 )
 
+
 class Room(models.Model):
-    type_room = models.CharField(max_length=100,choices=s)
+    type_room = models.CharField(max_length=100, choices=s)
     status = models.BooleanField(default=False)
     time = models.DateTimeField(auto_now=True)
-    time_left = models.CharField(max_length=100,choices=times)
-    order = models.ForeignKey(Menu,on_delete=models.CASCADE,blank=True)
+    time_left = models.CharField(max_length=100, choices=times)
+    order = models.ForeignKey(Menu, on_delete=models.CASCADE, blank=True)
+
 
 class Order(models.Model):
     name = models.CharField(max_length=100)
--- backend/gostinica/urls.py
+++ backend/gostinica/urls.py
@@ -4,9 +4,9 @@ from .views import *
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
-router.register('menu', RoomView)
-router.register('room', MenuView)
-router.register('order',OrderView)
+router.register('room', RoomView)
+router.register('menu', MenuView)
+router.register('order', OrderView)
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
